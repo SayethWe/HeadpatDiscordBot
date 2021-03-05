62b8cb6c88bb41d9f59e1091c7af8a90ee78298c
Sayeth_We <powerplanets@aol.com>
2021-03-04 21:49:32 -0500

    Add strict admin check capability
    whereas mod returns true in the case the user has certain moderation 
    prvieleges, admin will only return true if the user is marked as an 
    administrator

--- RoleControl.py
+++ RoleControl.py
@@ -10,6 +10,11 @@ def allowMod(message):
     is_mod = permissions.administrator or permissions.manage_guild or permissions.manage_messages
     return is_mod
 
+def allowAdmin(message):
+    permissions = message.channel.permissions_for(message.author)
+    is_admin = permissions.administrator
+    return is_admin
+
 ROLECONTROLDEFAULT = {
     '!usage' : allowAll,
     '!headpat' : allowAll,
