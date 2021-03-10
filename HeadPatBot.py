2f3f46c9ed6101fb36d8ce28e55ab1f3dd965951
Sayeth_We <powerplanets@aol.com>
2021-03-10 13:40:35 -0500

    move responses to seperate file

--- HeadPatBot.py
+++ HeadPatBot.py
@@ -3,6 +3,7 @@ import os
 import random
 import urllib
 import HelpFunctions as hf
+import responses
 import traceback
 import RoleControl as rc
 import discord
@@ -48,111 +49,7 @@ USAGE = {
     '!headpatremoveimage' : ("")
 }
 
-REPLY = {
-    'setimage' :
-    [
-        ("I'll never forgive you if the URL is broken or not wholesome, Baka!\n"
-         "I got rid of someone's image for you, too, so be thankful :relieved:")
-    ],
-    'setimagenone' :
-    [
-        ("There weren't any images to replace.\n"
-         "Were you making fun of me? You were, weren't you? :anger:\n"
-         "I've added your image anyway. It's not like it's a photo of me. Is it? :flushed:")
-    ],
-    'addimage' :
-    [
-        ("I know you wouldn't give me a not wholesome URL.\n"
-         "Would you?\n"
-         "Perhaps I'll just ignore it next time")
-    ],
-    'removeall' :
-    [
-        ("I will curse you until the end of your days.\n"
-         "They're gone.\n"
-         "All of them!")
-    ],
-    'urlbroken' :
-    [
-        ("That doesn't work!\n"
-         "If you don't get me a working URL, I, I'll... I'll have to do it, y'know")
-    ],
-    'existingurl' :
-    [
-        ("Are you making fun of me?\n"
-         "Even I can tell that I already have that one. :face_with_hand_over_mouth:\n"
-         "If you've got the time, maybe you should spend it on more worthwhile images! :anger:")
-    ],
-    'removeimage':
-    [
-        ("Ok, it's gone\n"
-         "It's not coming back...")
-    ],
-    'imagedne':
-    [
-        ("I don't have that one.\n"
-         "Do you? :face_with_hand_over_mouth:")
-    ],
-    'waifuaddexisting' :
-    [
-        ("They're in there, baka!\n"
-         "本当に何、この状況！:anger:")
-    ],
-    'waifuadd' :
-    [
-        ("Waifu Waifu Waifu! More Waifu!\n"
-         "They've been added to the pool")
-    ],
-    'waifuRemove' :
-    [
-        ("They're Gone.\n"
-         "*Less competition, I suppose.*"),
-        ("I removed that inferior choice for you.\n"
-         "Pick better next time, okay?")
-    ],
-    'waifustartpoll' :
-    [
-        ("Just a sec...")
-    ],
-    'waifuendpoll' :
-    [
-        ("Wait a bit...")
-    ],
-    'unhandled' :
-    [
-        ("Something happened, report this to the devs\n"
-         "https://github.com/SayethWe/HeadpatDiscordBot/issues")
-    ],
-    'unfinishedpoll' :
-    [
-        ('Nah uh uh, you have an unfinsihed poll, call !waifu endpoll starting a new one')
-    ],
-    'novalidpicks' :
-    [
-        ("Not enough waifu, MOAR needed.\n"
-         "Your waifus need a break, you know.")
-    ],
-    'endedpoll' :
-    [
-        ("We already recorded and finished the poll. You can use !waifu pollresults to get results from now on")
-    ],
-    'nopollmade' :
-    [
-        ("You gotta start a poll first")
-    ],
-    'polldne' :
-    [
-        ("The message with the previous poll might have been deleted, I can't find it")
-    ],
-    'forbidden' :
-    [
-        ("You can't do that, go ask a mod")
-    ],
-    'waifuaddcsv' :
-    [
-        ("Waifus added. So much waifu. Fwoooooo")
-    ]
-}
+
 
 ERRORS_HANDLED = {
     'MissingRequiredArgument' : "That's not enough information! Come back when you have more",
@@ -457,6 +354,6 @@ def verifyURL(url):
         return False
 
 def getResponse(key : str):
-    return random.choice(REPLY[key])
+    return random.choice(responses.REPLY[key])
 
 bot.run(TOKEN)
